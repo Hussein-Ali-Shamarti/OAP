b6e0ccb385a79d9d8cc6879c1f6e61913bb794e9
Hussein Ali Al-Shamarti <113942564+Hussein-Ali-Shamarti@users.noreply.github.com>
2023-11-16 14:17:08 +0100

    Add files via upload

---
+++ OAP/src/main/java/mysql_connection/java/OAP/App.java
@@ -0,0 +1,13 @@
+package mysql_connection.java.OAP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
