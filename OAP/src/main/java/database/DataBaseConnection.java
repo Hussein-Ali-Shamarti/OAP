293ffd783eb13e01868c9b0e759a0a3071a021f5
AlbertAsmervik <Albertasmervik@gmail.com>
2023-11-18 11:27:43 +0100

    structure

---
+++ OAP/src/main/java/database/DataBaseConnection.java
@@ -0,0 +1,27 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    protected static String dbUrl = "jdbc:mysql://127.0.0.1:3306/classicmodels";
+    protected static String user = "student";
+    protected static String password = "student";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, user, password);
+    }
+
+    public static String getDbUrl() {
+        return dbUrl;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
