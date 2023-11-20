07007da367d4db7c5acbc73620ed7fd90970890c
OleSun <127397159+OleSun@users.noreply.github.com>
2023-11-20 11:52:11 +0100

    Update ConnectionInterface.java

--- OAP/src/main/java/database/ConnectionInterface.java
+++ OAP/src/main/java/database/ConnectionInterface.java
@@ -1,5 +1,41 @@
+
+/**
+ * Declaration of the methods for establishing database connection the interface
+ * provides all necessary methods that should be implemented to achieve
+ * successful connection to a database
+ */
+
 package database;
 
-public class ConnectionInterface {
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+
+public interface ConnectionInterface {
+	
+	
+
+		/**
+		 * Open connection
+		 * 
+		 * @throws SQLException
+		 */
+		void open() throws SQLException;
+
+		/**
+		 * Close connection
+		 * 
+		 * @throws SQLException
+		 */
+		void close() throws SQLException;
+
+		/**
+		 * Test connection
+		 * 
+		 * @throws SQLException
+		 */
+		void test() throws SQLException;
 
 }
