6d428bd73427303c1b166e9ac979dea5d1473075
AlbertAsmervik <Albertasmervik@gmail.com>
2023-11-21 19:02:20 +0100

    fix

--- OAP/src/main/java/controller/CustomerHandler.java
+++ OAP/src/main/java/controller/CustomerHandler.java
@@ -20,24 +20,36 @@ import database.DataBaseConnection;
 
 public class CustomerHandler {
 	
-    public static boolean addCustomer(int customerNr, String companyName, String contactLastName, String contactFirstName, int salesRepEmployeeNr, BigDecimal creditLimit) {
-        try (Connection connection = DataBaseConnection.getConnection();
-             PreparedStatement pstm = connection.prepareStatement("INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, salesRepEmployeeNumber, creditLimit) VALUES (?, ?, ?, ?, ?, ?, )")) {
-            pstm.setInt(1, customerNr);
-            pstm.setString(2, companyName);
-            pstm.setString(3, contactLastName);
-            pstm.setString(4, contactFirstName);
-            pstm.setInt(5, salesRepEmployeeNr);
-            pstm.setBigDecimal(6, creditLimit);
-
-            int affectedRows = pstm.executeUpdate();
-
-            return affectedRows > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	public static boolean addCustomer(int customerNr, String companyName, String contactLastName, String contactFirstName, 
+            String phone, String addressLine1, String addressLine2, String city, 
+            String state, String postalCode, String country, 
+            int salesRepEmployeeNr, BigDecimal creditLimit) {
+		try (Connection connection = DataBaseConnection.getConnection();
+		PreparedStatement pstm = connection.prepareStatement(
+		"INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, " + 
+		"phone, addressLine1, addressLine2, city, state, postalCode, country, " +
+		"salesRepEmployeeNumber, creditLimit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+		pstm.setInt(1, customerNr);
+		pstm.setString(2, companyName);
+		pstm.setString(3, contactLastName);
+		pstm.setString(4, contactFirstName);
+		pstm.setString(5, phone);
+		pstm.setString(6, addressLine1);
+		pstm.setString(7, addressLine2);
+		pstm.setString(8, city);
+		pstm.setString(9, state);
+		pstm.setString(10, postalCode);
+		pstm.setString(11, country);
+		pstm.setInt(12, salesRepEmployeeNr);
+		pstm.setBigDecimal(13, creditLimit);
+		
+		int affectedRows = pstm.executeUpdate();
+		return affectedRows > 0;
+		} catch (SQLException e) {
+		e.printStackTrace();
+		return false;
+		}
+}
 
     public boolean editCustomer(int customerNr, String companyName, String contactLastName, String contactFirstName, int salesRepEmployeeNr, BigDecimal creditLimit) {
         try (Connection connection = DataBaseConnection.getConnection();
