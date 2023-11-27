9babc2455e935680814f7226eae82ecdf027b880
OleSun <127397159+OleSun@users.noreply.github.com>
2023-11-27 15:01:56 +0100

    Update EmployeesListener.java

--- OAP/src/main/java/Listners/EmployeesListener.java
+++ OAP/src/main/java/Listners/EmployeesListener.java
@@ -1,5 +1,22 @@
 package Listners;
 
-public class EmployeesListener {
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingUtilities;
+
+import view.EmployeeView;
+
+class EmployeesListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Open EmployeeView when "Employees" button is pressed
+    	 SwingUtilities.invokeLater(() -> {
+             EmployeeView employeeView = new EmployeeView();
+             employeeView.setVisible(true);
+    	 });
+    	 
+    }
+    
 }
